0a55e90562c1e8ca59439b3fa1eec151789c7e21
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2019-06-16 17:29:28 -0300

    setup dotenv lib and new env variables in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,19 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-mongoose.connect('mongodb://localhost:27017/instagram', {
+mongoose.connect(process.env.URI_MONGO, {
   useNewUrlParser: true,
 });
 
 app.use((req, res, next) => {
   req.io = io;
-
   next();
 });
 
@@ -27,4 +29,4 @@ app.use(
 
 app.use(require('./routes'));
 
-server.listen(3333);
+server.listen(process.env.PORT);
